4e810b55026733ddd53ab9d879d535935e8346bc
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-18 14:23:08 +0530

    Create sacchin.c

---
+++ Beginner level/sacchin.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char s[30]="Sachin Ramesh Tendulkar";
+char s1[20]="Little Master";
+int runs=18426;
+float bavg=44.83;
+int matches=463;
+printf("%s",s);
+printf("\n%s",s1);
+printf("\n%d",runs);
+printf("\n%f",bavg);
+printf("\n%d",matches);
+return 0;
+}
