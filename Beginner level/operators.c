04b834c532c27fac41694c446b574afe07af6727
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-18 16:05:56 +0530

    Create operators.c

---
+++ Beginner level/operators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a=5;
+int b=5;
+int sum,dif,mul,div,mod;
+sum=a+b;
+dif=a-b;
+mul=a*b;
+div=a/b;
+mod=a%b;
+printf("%d,%d,%d,%d,%d",sum,diff,mul,div,mod);
+
+return 0;
+}
