271da7d1aea5b5728c63fdcbe6d50fd06574d286
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-18 19:40:16 +0530

    Create arguments.c

---
+++ Beginner level/arguments.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main( int argc, char *argv[] )
+{if( argc == 2 )
+{
+printf("The argument supplied is %s
+", argv[1]);
+}
+else if( argc > 2 ){
+printf("Too many arguments supplied.
+");
+}
+else{
+printf("One argument expected.");
+
+}}
