39af235df118c99f6db80222638a02941e5d5864
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-19 13:01:28 +0530

    Update vowel.c

--- assignments/vowel.c
+++ assignments/vowel.c
@@ -3,10 +3,10 @@ int main()
 {
 char v;
 printf("\n enter the character");
-scanf("%c",v);
-if((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u')||(c=='A')||(c=='E')||(c=='I')||(c=='O')||(c=='U'))
+scanf("%c",&v);
+if((v=='a')||(v=='e')||(v=='i')||(v=='o')||(v=='u')||(v=='A')||(v=='E')||(v=='I')||(v=='O')||(v=='U'))
 printf("\n vowel");
 else
-printf("\n consonant);
+printf("\n consonant");
 return 0;
 }
