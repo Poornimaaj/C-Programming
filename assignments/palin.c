3ce755925f8f252a61fe5d13541bd6703dc2b8d5
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-19 21:26:33 +0530

    Update palin.c

--- assignments/palin.c
+++ assignments/palin.c
@@ -1,16 +1,27 @@
-#include<stdio.h>
+
+
+#include <stdio.h>
+ 
 int main()
 {
-int n,n1;
-scanf("%d",&n);
-while(n)
-{
-n=n%10;
-printf("%d",n);
-}
-if(n==n)
-printf("palindrome");
-else
-printf("not a palindrome");
-return 0;
+   int n, rev = 0, integer;
+ 
+   printf("Enter a number to check if it is a palindrome or not\n");
+   scanf("%d",&n);
+ 
+   integer = n;
+ 
+   while(integer>0)
+   {
+      rev = rev*10;
+      rev = rev + integer%10;
+      integer = integer/10;
+   }
+ 
+   if ( n == rev )
+      printf("%d is a palindrome number, n);
+   else
+      printf("%d is not a palindrome number, n);
+ 
+   return 0;
 }
