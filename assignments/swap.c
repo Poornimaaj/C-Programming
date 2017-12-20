5e13b069673f7a6505afad777bb10a431382c022
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-20 14:43:51 +0530

    Create swap.c

---
+++ assignments/swap.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s[100],t; 
+int len,i;
+printf("\n enter the string");
+scanf("%s",s);
+len=strlen(s);
+if(len%2==0)
+{
+for(i=0;i<len;i+=2)
+{
+t=s[i];
+s[i]=s[i+1];
+s[i+1]=t;
+
+}
+printf("%s",s);
+}
+else
+{
+for(i=0;i<(len-1);i+=2)
+{
+t=s[i];
+s[i]=s[i+1];
+s[i+1]=t;
+
+}
+printf("%s",s);
+}
+return 0;
+}
