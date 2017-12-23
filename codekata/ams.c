5641a895817ccfe80c670162d8edf580d4aa4db1
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-23 15:40:20 +0530

    Create ams.c

---
+++ codekata/ams.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int number, originalNumber, remainder, result = 0;
+
+    printf("Enter a three digit integer: ");
+    scanf("%d", &number);
+
+    originalNumber = number;
+
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber%10;
+        result += remainder*remainder*remainder;
+        originalNumber /= 10;
+    }
+
+    if(result == number)
+        printf("yes");
+    else
+        printf("no");
+
+    return 0;
+}
