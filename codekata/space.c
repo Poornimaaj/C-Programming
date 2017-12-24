4aaa4309e5ec700926944f428d2b0cab22879551
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-24 19:39:56 +0530

    Create space.c

---
+++ codekata/space.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[100];
+    int l,count=0,i,c;
+    scanf("%s",s);
+    l=strlen(s);
+    for(i=0;i<l;i++)
+     {
+        if(s[i]==' ')
+        count++;
+        
+    }
+   
+    c=l-count;
+    printf("%d",c);
+    
+}
