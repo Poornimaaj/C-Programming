a1cb63d330f3a2c8ae17f5e9f9726db6cd541054
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-25 09:57:55 +0530

    Create pattern.c

---
+++ SET 4/pattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   char s[100];
+   int n,i;
+   scanf("%s",s);
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       printf("\n %s",s);
+      
+   }
+   
+    return 0;
+}
