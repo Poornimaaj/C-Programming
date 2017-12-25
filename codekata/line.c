dc8757994abccecfb076fe5ab9988e8f02d54519
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-25 08:56:09 +0530

    Create line.c

---
+++ codekata/line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   char s[100];
+   int l,count=1,i;
+   scanf("%s,s");
+   l=strlen(s);
+   for(i=0;i<l;i++)
+   {
+       if(s[i]=='.')
+       count++;
+       printf("%d",count);
+       else
+       printf("%d",count);
+   }
+
+    return 0;
+}
+
+
