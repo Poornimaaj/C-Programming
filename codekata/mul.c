bb33ff092dc16031497c2700fc1e59beebd47286
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-03 21:38:43 +0530

    Create mul.c

---
+++ codekata/mul.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+    int main()
+    {
+        int i,n,mul;
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+            mul=n*i;
+        
+        printf("%d  ",mul);
+        }
+        return 0;
+    }
+
+    
