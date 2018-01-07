126caa73ff3aab1cafe77bd9bcf95aa10a8fe48f
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-07 19:42:08 +0530

    Update fibo.c

--- SET 4/fibo.c
+++ SET 4/fibo.c
@@ -2,14 +2,14 @@
 #include<string.h>
 int main()
 {
-   int a=0,b=1,c,i,n;
+   int x=0,y=1,z,i,n;
    scanf("%d",&n);
    for(i=1;i<=n;i++)
    {
-       printf("%d ",a);
-       c=a+b;
-       a=b;
-       b=c;
+       printf("%d ",x);
+       z=x+y;
+      x=y;
+      y=z;
    }
    
     return 0;
