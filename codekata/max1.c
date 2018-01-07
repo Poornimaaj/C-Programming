3c97ddf526fd90f2c746461e015693c1a18a4d4a
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-07 20:06:50 +0530

    Update max1.c

--- codekata/max1.c
+++ codekata/max1.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int n,max=0,i,a[10];
+int n,max=0,i,arr[10];
 max=a[0];
 
 for(i=0;i<10;i++)
 {
-scanf("%d",&a[i]);
+scanf("%d",&arr[i]);
 }
 for(i=0;i<n;i++)
 {
-if(a[i]>max)
-max=a[i];
+if(arr[i]>max)
+max=arr[i];
 }
 printf("%d",max);
 return 0;
