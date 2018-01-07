f458841c15790f305862b9e37e45bca60f8439f7
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-07 20:11:57 +0530

    Update and rename SWAP.C to SWAP1.C

---
+++ SET 4/SWAP1.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   int x,y,t;
+   scanf("%d %d",&x,&y);
+   if(a!=0)
+   {
+       t=x;
+       x=y;
+       y=t;
+   }
+   printf("%d %d",x,y);
+    return 0;
+}
