7f532b26bd655e512eb7cb79b7decf6c8fdf3edf
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-15 19:06:23 +0530

    Rename num_words.c to num1_words.c

---
+++ SET-6/num1_words.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	char a[100];
+	int n,i,count=0;
+	scanf("%[^\t\n]s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]==' ')
+	    {   
+	        count++;
+	    }
+	    else
+	    {
+	        continue;
+	    }
+	}
+    printf("%d",count+1);
+	return 0;
+}
