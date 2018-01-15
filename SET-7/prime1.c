a3517f82055caa4c0c96b67bf5c1b6655721b1d3
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-15 19:22:51 +0530

    Create prime1.c

---
+++ SET-7/prime1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    int n,i,count=0;
+    scanf("%d",&n);
+    for(i=2;i<=(n/2);i++)
+    {
+        if(n%i==0)
+            count++;
+    }
+    if(count==0)
+        printf("yes");
+    else
+        printf("no");
+	return 0;
+}
