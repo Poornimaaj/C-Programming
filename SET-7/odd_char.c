e7e0667122d795018e7db3649471bc05946607fa
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-16 19:30:05 +0530

    Create odd_char.c

---
+++ SET-7/odd_char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+ 
+ char s[100];
+ int l,i;
+ scanf("%s",s);
+ l=strlen(s);
+ for(i=0;i<l;i++)
+ {
+     if((s[i]%2)!=0)
+     printf("%c ",s[i]);
+ }
+  return 0;
+}
+
