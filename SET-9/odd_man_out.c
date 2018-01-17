7d13f1cf4395af238410010d8355ef72257a71b6
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-17 20:20:02 +0530

    Create odd_man_out.c

---
+++ SET-9/odd_man_out.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int n,i,a[10];
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+      scanf("%d",&a[i]);
+      if(a[i]!=i)
+      {
+          printf("%d",i);
+      }
+  }
+   return 0;
+}
