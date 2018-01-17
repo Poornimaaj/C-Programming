dc0bff6aea791a84dac90dda439c0b870e2d443d
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-17 19:19:12 +0530

    Create position.c

---
+++ SET-9/position.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a[10],i,p,sum=0;
+    scanf("%d %d",&n,&p);
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    
+    }
+    printf("%d",a[p-1]);
+    return 0;
+}
