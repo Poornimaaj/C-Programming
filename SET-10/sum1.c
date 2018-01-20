2c0b455b35758f0785c2849f817a84510ed820e0
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-01-20 20:26:48 +0530

    Create sum1.c

---
+++ SET-10/sum1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10],i,n,sum=0;
+    
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+   
+    sum=sum+i;
+    
+    }
+    
+    printf("%d",sum);
+
+    return 0;
+}
