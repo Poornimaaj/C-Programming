72ee667c34f138b1006c691a2d87383529b330f2
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-02-06 21:40:27 +0530

    Create points_same_plane.c

---
+++ PLAYER_LEVEL/SET-3/points_same_plane.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int a[2],b[2],c[2],i,flag=0;
+	for(i=0;i<2;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	for(i=0;i<2;i++)
+	{
+	scanf("%d",&b[i]);
+	}
+	for(i=0;i<2;i++)
+	{
+	scanf("%d",&c[i]);
+	}
+
+	for(i=0;i<2;i++)
+	{
+	if(a[0]==b[0] && b[0]==c[0])
+    flag=1;
+	}
+	if(flag==1)
+	printf("yes");
+	return 0;
+}
+
+   
