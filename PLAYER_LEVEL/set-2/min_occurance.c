2f8177c9129f9b0f11529edd5c0ce06b61666daa
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-02-06 20:27:42 +0530

    Create min_occurance.c

---
+++ PLAYER_LEVEL/set-2/min_occurance.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+	int a[10],ch,n,i,j,max=0,count=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                count++;
+                a[j]='+';
+            }
+        }
+        if(count<=max)
+        {
+            max=count;
+            ch=a[i];
+        }
+    }
+    printf("%d",ch);
+	return 0;
+}
+
