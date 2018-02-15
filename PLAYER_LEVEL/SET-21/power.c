9a82a06c2714435a9c7d9fde1f18fbad1f767cb2
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-02-15 20:34:01 +0530

    Create power.c

---
+++ PLAYER_LEVEL/SET-21/power.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,p=1,sum=0,i;
+    scanf("%d %d",&a,&b);
+    for(i=0;i<b;i++)
+    {
+        p=p*a;
+    
+    }
+    printf("%d",p);
+    return 0;
+}
