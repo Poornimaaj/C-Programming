c70f724a338565b57302e5c2a230eeab3eea3422
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-02-15 20:43:26 +0530

    Create (a^b)%c .c

---
+++ PLAYER_LEVEL/SET-21/(a^b)%c .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,p=1,sum=0,i,r,c;
+    
+    scanf("%d %d %d",&a,&b,&c);
+    for(i=0;i<b;i++)
+    {
+        p=p*a;
+    
+    }
+    r=p%c;
+    printf("%d",r);
+   
+}
