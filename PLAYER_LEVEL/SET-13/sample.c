063294f174d71f0de9aaccc48e6e35be1c84bd6c
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-03-02 19:47:31 +0530

    Create sample.c

---
+++ PLAYER_LEVEL/SET-13/sample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    int n,i;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+    gets(str);
+   
+    }
+    printf("%s",str);
+    return 0;
+}
+
+
