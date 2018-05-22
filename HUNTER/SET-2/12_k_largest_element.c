8af5a5b0dd1fa060fe39e7258003499a15f52147
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-05-22 19:54:22 +0530

    Create 12_k_largest_element.c

---
+++ HUNTER/SET-2/12_k_largest_element.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j,a[10],t;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+        
+    }
+     printf("%d",a[k-1]);
+}
