be49143aab5c7587d2b3f5d72002c6c13bf2289f
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-06-06 09:20:33 +0530

    Create 50_minimum_divisors_of_number.c

---
+++ PLAYER_LEVEL/SET-5/50_minimum_divisors_of_number.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main()
+{
+     int n,i,count=0;
+	scanf("%d",&n);
+   for(i=2;i<n;i++)
+   {
+       if(n%i==0)
+       count++;
+   }
+   if(count>0)
+   printf("yes");
+   else
+   printf("no");
+	return 0;
+}
