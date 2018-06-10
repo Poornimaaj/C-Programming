53b35cdee0f5eb7447adc094937e00d9128f655d
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-06-10 20:11:25 +0530

    Create 82_bitwise_AND.c

---
+++ PLAYER_LEVEL/SET-9/82_bitwise_AND.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a[10],i,res=0,j,n;
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+     {
+    for(j=i+1;j<n;j++)
+    {
+    res=res+(a[i] & a[j]);
+    }
+    }
+	printf("%d",res);
+	return 0;
+}
