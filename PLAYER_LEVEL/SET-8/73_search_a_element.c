599d85a8e893a7f0a1f0b2335025c380c815664a
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-06-10 12:44:10 +0530

    Create 73_search_a_element.c

---
+++ PLAYER_LEVEL/SET-8/73_search_a_element.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,a[10],k;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       if(a[i]==k)
+        printf("%d",i+1);
+    }
+    return 0;
+}
