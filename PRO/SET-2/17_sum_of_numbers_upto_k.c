faa5fda7344569021f92e563f3924d5e4d605716
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-06-12 20:15:32 +0530

    Create 17_sum_of_numbers_upto_k.c

---
+++ PRO/SET-2/17_sum_of_numbers_upto_k.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,a[10],i,j,flag=0;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            
+            if(a[i]+a[j]==k)
+            flag=1;
+        }
+    }
+    if(flag==1)
+    printf("yes");
+    else
+    printf("no");
+    return 0;
+    
+}
