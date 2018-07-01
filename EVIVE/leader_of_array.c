260641034ff2c771c636d45303484ac5bbdad08a
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-07-01 16:15:12 +0530

    Create leader_of_array.c

---
+++ EVIVE/leader_of_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a[n],i,j,count;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        count=0;
+       for(j=i+1;j<n;j++)
+       {
+           if(a[i]>a[j])
+           count++;
+           if(count==((n-i)-1))
+           printf("%d ",a[i]);
+       }
+    }
+    printf("%d",a[n-1]);
+
+    return 0;
+}
