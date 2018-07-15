13ffca6597733808e280809731cc83a7457bbdd5
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-07-15 12:45:11 +0530

    Update 22_max_sum_without_neighbours.java

--- PRO/SET-3/22_max_sum_without_neighbours.java
+++ PRO/SET-3/22_max_sum_without_neighbours.java
@@ -2,26 +2,16 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-	    Scanner scan=new Scanner(System.in);
-	    int i,j,sum=0,k=0,l;
-		int n=scan.nextInt();
+        Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
 		int a[]=new int[n];
-		int b[]=new int [n];
-		for(i=0;i<n;i++)
+		for(int i=0;i<n;i++)
 		{
-		    a[i]=scan.nextInt();
+			a[i]=sc.nextInt();
 		}
-		for(i=0;i<n;i++)
-		{
-		    sum=0;
-		    for(j=i;j<n;j=j+2)
-		    {
-		        sum=sum+a[j];
-		    }
-		    b[k]=sum;
-		    k++;
-		}
-		Arrays.sort(b);
-		System.out.println(b[k-1]);
+	      
+	        Arrays.sort(a);
+       int b=a[n-1]+a[n-2] ; 
+     System.out.println(b);
 	}
 }
