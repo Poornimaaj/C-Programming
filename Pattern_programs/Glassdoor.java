657932c1551f35c4c67ff8c25fab47954e7b932e
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-07-26 19:14:05 +0530

    Create Glassdoor.java

---
+++ Pattern_programs/Glassdoor.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int i,j;
+	for(i=1;i<=5;i++)
+	{
+	        System.out.print(i);
+	        System.out.print(" ");
+	    
+	}
+	System.out.println();
+	for(j=2;j<=5;j++)
+	{
+	    
+	    System.out.println(j);
+	}
+		for(i=1;i<=5;i++)
+	{
+	        System.out.print(i);
+	        System.out.print(" ");
+	    
+	}
+	}
+}
