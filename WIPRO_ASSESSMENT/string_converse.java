f1d373f79e3b67772cbb3e1e90d794295d386fad
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2018-08-14 20:46:43 +0530

    Create string_converse.java

---
+++ WIPRO_ASSESSMENT/string_converse.java
@@ -0,0 +1,70 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan=new Scanner(System.in);
+	int l,l1,i,j,k,a,b,t,t1;
+	String s[]=scan.nextLine().split(" ");
+	int n=scan.nextInt();
+	String r="";
+	String r1="";
+	t=n/10;
+	t1=n%10;
+	for(i=0;i<s.length;i++)
+	{
+	    String str=s[t-1];
+	    l=str.length();
+	    if(l%2==0)
+	    {
+	    for(j=l/2-1;j>=0;j--)
+	    {
+	        r=r+str.charAt(j);
+	    }
+	    for(k=l/2;k<l;k++)
+	    {
+	        r=r+str.charAt(k);
+	    }
+	    }
+	    else
+	    {
+	        for(j=l/2;j>=0;j--)
+	    {
+	        r=r+str.charAt(j);
+	    }
+	    for(k=l/2;k<l;k++)
+	    {
+	        r=r+str.charAt(k);
+	    }
+	        
+	    }
+	    String str1=s[t1-1];
+	    l1=str1.length();
+	    if(l1%2==0)
+	    {
+	    for(a=l1/2-1;a>=0;a--)
+	    {
+	        r1=r1+str1.charAt(a);
+	    }
+	    for(b=l1/2;b<l1;b++)
+	    {
+	        r1=r1+str1.charAt(b);
+	    }
+	    }
+	    else
+	    {
+	        for(a=l1/2;a>=0;a--)
+	    {
+	        r1=r1+str1.charAt(a);
+	    }
+	    for(b=l1/2;b<l1;b++)
+	    {
+	        r1=r1+str1.charAt(b);
+	    }
+	        
+	    }
+	}
+	    System.out.println(r);
+	    System.out.println(r1);
+	}
+	}
+
