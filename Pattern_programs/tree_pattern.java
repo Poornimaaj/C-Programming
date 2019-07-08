94bc513b72b727a22a5130dd43ad56b1c69753d2
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2019-07-08 20:43:55 +0530

    Create tree_pattern.java

---
+++ Pattern_programs/tree_pattern.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	 for(int i=1;i<=5;i++)
+	 {
+	     for(int j=i;j<5;j++)
+	     {
+	         System.out.print(" ");
+	     }
+	     for(int k=1;k<(2*i);k++)
+	     {
+	         System.out.print("* ");
+	     }
+	     System.out.println("");
+	 }
+	
+	    
+	
+	 
+	
+	
+	}
+}
