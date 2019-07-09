3addd4bae1d03835d0694fb9acf65c2792289142
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2019-07-09 20:30:21 +0530

    Create permutation.java

---
+++ PLAYER_LEVEL/SET-9/permutation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int p=1,t=0,i;
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        if(a==0||b==0)
+        System.out.println("0");
+        else
+        {
+        for(i=0;i<b;i++)
+        {
+            p=p*a;
+            a--;
+        }
+       
+        System.out.println(p);
+        }
+	}
+}
